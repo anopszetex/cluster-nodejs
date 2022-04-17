0fbb1f2b10559244e0772257454b2e721e8e500a
Andre Luis <andre.1357.luis@gmail.com>
2022-04-17 11:16:23 -0300

    🎉 init

---
+++ src/server.js
@@ -0,0 +1,14 @@
+import { startServer } from './server/index.js'
+import dotenv from "dotenv"
+dotenv.config()
+
+function initializeServer () {
+  try {
+    startServer()
+    console.log(`🚀 Server running and pid at ${process.pid}`)
+  } catch (err) {
+    console.error(`❌ Fail to start server: ${err.message}`)
+  }
+}
+
+initializeServer()
---
+++ src/server/index.js
@@ -0,0 +1,35 @@
+import { createServer } from 'http'
+import { appendFile } from 'fs/promises'
+import { fileManager, serverConfig } from './config.js'
+
+const SIZE = { length: 1e3 }
+
+//* 0 -> 40
+function randomNumber (max = 40) {
+  return Math.floor(Math.random() * max)
+}
+
+function applyTotal () {
+  const list = Array.from(SIZE, () => randomNumber())
+  
+  const total = list.reduce((acc, element) => acc + element, 0)
+
+  return total
+}
+
+async function handler (request, response) {  
+  await appendFile(fileManager.root, `Processed by ${process.pid} \n`)
+    
+  const result = applyTotal()
+
+  response.end(result.toString())
+}
+
+function startServer () {
+  const server = createServer(handler)
+
+  server.listen(serverConfig.port)  
+}
+
+
+export { startServer }
